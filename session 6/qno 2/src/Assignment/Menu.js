1dcbd6d0872cc5fd849626c6b4fb66fccec20318
ShivanandaSTeradal <90270309+ShivanandaSTeradal@users.noreply.github.com>
2021-10-05 21:36:12 +0530

    Add files via upload

---
+++ session 6/qno 2/src/Assignment/Menu.js
@@ -0,0 +1,30 @@
+const Menu = [
+    {
+      id: 1,
+      name: "Dal",
+      type: "Veg",
+      
+    },
+  
+    {
+      id: 2,
+      name: "Fish",
+      type: "Non-veg",
+      
+    },
+  
+    {
+      id: 3,
+      name: "Meat",
+      type: "Non-veg",
+     
+    },
+  
+    {
+      id: 4,
+      name: "Rice",
+      type: "Veg",
+     
+    }
+  ];
+  export default Menu;
