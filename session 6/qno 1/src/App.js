a65694b46f414dc36f563fe62192c25e4661c996
ShivanandaSTeradal <90270309+ShivanandaSTeradal@users.noreply.github.com>
2021-10-05 12:01:32 +0530

    Add files via upload

---
+++ session 6/qno 1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import List from './Assignment/List';
+
+function App() {
+  return (
+    <div className="container">
+    <List/>
+     </div>
+  );
+}
+export default App;
